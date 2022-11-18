7f18ccdbf92a1fc2639d7accd57f2bfb7de4861f
arguhuh <hassard.patrick@gmail.com>
2022-11-19 00:36:20 +1000

    updated template

--- 2021/code00.py
+++ 2021/code00.py
@@ -1,9 +1,9 @@
-# infile = open("input/in#_real.txt","r")
-infile = open("input/in#_test.txt","r")
+# infile = open("input/in##_real.txt","r")
+infile = open("input/in##_test.txt","r")
 L = [line for line in infile]
 
-def day#_part1(L):
+def day##_part1(L):
 	return L
 
-print(day#_part1(L))
-# print(day#_part2(L))
+print(day##_part1(L))
+# print(day##_part2(L))
