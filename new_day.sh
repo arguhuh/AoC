8ed8a311c66ba5629333f18a8ac4ba4c459ec778
arguhuh <hassard.patrick@gmail.com>
2022-12-02 16:17:51 +1000

    updated bash script

--- new_day.sh
+++ new_day.sh
@@ -6,9 +6,11 @@ then
 	touch input/in$1_test.txt
 	cp -v code00.py code$1.py
 	sed -i "s/##/$1/g" code$1.py
+	unix2dos code$1.py
 else
 	touch input/in0$1_real.txt
 	touch input/in0$1_test.txt
 	cp -v code00.py code0$1.py
 	sed -i "s/##/0$1/g" code0$1.py
+	unix2dos code0$1.py
 fi
